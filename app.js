af1bf6d0ef3e6990d83269083718ef2feb05afc8
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-10-17 20:18:46 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(8080, () => {
+    console.log('Started Server')
+})
