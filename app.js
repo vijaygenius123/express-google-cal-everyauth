ebd66f8ab0004f1a50faa9adc995597deabccf62
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-10-18 18:01:56 +0100

    Add deps and basic setup

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 const express = require('express');
+const mustacheExpress = require('mustache-express');
 const logger = require('morgan');
+const cookieSession = require('cookie-session');
+const everyauth = require('@fusebit/everyauth-express');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
+app.engine('mustache', mustacheExpress());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/views');
+app.use(cookieSession({ name: 'session', secret: 'secret' }));
+
+app.use(
+    "/google/authorize/:userId",
+    everyauth.authorize("google", {
+        // EveryAuth will automatically redirect to this route after authenticate
+        finishedUrl: "/google/calendarlist",
+        // The user ID of the authenticated user the credentials will be associated with
+        mapToUserId: (req) => req.params.userId,
+    })
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+
+
 
 app.listen(8080, () => {
     console.log('Started Server')
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    return res.render("index", {userId: uuidv4()});
 });
 
 module.exports = router;
